700585ef90ec7ddc24a63c326c85816dad186ab8
Luis Angel <angelfgdeveloper@gmail.com>
2024-04-07 12:21:51 -0600

    Encriptar contraseña

--- src/presentation/services/auth.service.ts
+++ src/presentation/services/auth.service.ts
@@ -1,3 +1,4 @@
+import { bcryptAdapter } from '../../config';
 import { UserModel } from '../../data';
 import { CustomError, RegisterUserDto, UserEntity } from '../../domain';
 
@@ -15,9 +16,10 @@ export class AuthService {
     try {
       
       const user = new UserModel(registerUserDto);
-      await user.save();
-
+      
       // Encriptar contraseña
+      user.password = bcryptAdapter.hash(registerUserDto.password);
+      await user.save();
 
       // Generar JWT
 
