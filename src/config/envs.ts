7b242620745741b69c807322cbfd4b6f0b4c777a
Luis Angel <angelfgdeveloper@gmail.com>
2024-05-19 14:37:44 -0600

    Deshabilitando el envio de emails en variables de entorno

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -15,6 +15,8 @@ export const envs = {
   MAILER_SECRET_KEY: get('MAILER_SECRET_KEY').required().asString(),
 
   WEBSERVICE_URL: get('WEBSERVICE_URL').required().asString(),
+
+  SEND_EMAIL: get('SEND_EMAIL').default('false').asBool(),
   
 }
 
