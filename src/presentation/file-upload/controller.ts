47ff42293d5aa474c304fc60280dd5471d46f78b
Luis Angel <angelfgdeveloper@gmail.com>
2024-08-18 11:18:47 -0600

    Base de rutas de archivos individuales y multiples

---
+++ src/presentation/file-upload/controller.ts
@@ -0,0 +1,48 @@
+import { Response, Request } from 'express';
+import { CustomError } from '../../domain';
+
+export class FileUploadController {
+
+  // DI
+  constructor(
+    // private readonly fileUploadService: FileUploadService,
+  ) { }
+
+  private handleError = ( error: unknown, res: Response ) => {
+    if ( error instanceof CustomError ) {
+      return res.status( error.statusCode ).json( { error: error.message } );
+    }
+
+    console.log( `${ error }` );
+    return res.status( 500 ).json( { error: 'Internal server error' } );
+  };
+
+  uploadFile = ( req: Request, res: Response ) => {
+
+    const type = req.params.type;
+    // const file = req.body.files.at(0) as UploadedFile;
+
+    
+    // this.fileUploadService.uploadSingle( file, `uploads/${ type }` )
+    //   .then( uploaded => res.json(uploaded) )
+    //   .catch(  error => this.handleError( error, res ) )
+
+    res.json('uploadFile');
+
+  };
+  
+  uploadMultileFiles = ( req: Request, res: Response ) => {
+
+    const type = req.params.type;
+    // const files = req.body.files as UploadedFile[];
+
+    
+    // this.fileUploadService.uploadMultiple( files, `uploads/${ type }` )
+    //   .then( uploaded => res.json(uploaded) )
+    //   .catch(  error => this.handleError( error, res ) )
+
+    res.json('uploadMultipleFile');
+
+  };
+
+}
---
+++ src/presentation/file-upload/routes.ts
@@ -0,0 +1,26 @@
+import { Router } from 'express';
+import { FileUploadController } from './controller';
+
+export class FileUploadRoutes {
+
+  static get routes(): Router {
+
+    const router = Router();
+    const controller = new FileUploadController(
+      // new FileUploadService()
+    );
+
+    // router.use( FileUploadMiddleware.containFiles );
+    // router.use( TypeMiddleware.validTypes(['users','products','categories']) );
+
+    // Definir las rutas
+    // api/upload/single/<user|category|product>/
+    // api/upload/multiple/<user|category|product>/
+    router.post('/single/:type', controller.uploadFile);
+    router.post('/multiple/:type', controller.uploadMultileFiles);
+
+    return router;
+  }
+
+}
+
